7497785b3d23c70eb71ecabab757f9989ff1b628
ernesthenry <henry38ernest@gmail.com>
2022-10-15 14:40:45 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+class Employee:
+    EmpCount = 0
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+        Employee.EmpCount += 1
+
+    def displayCount(self):
+        print("Employee count %d" % Employee.EmpCount)
+
+    def displayEmployee(self):
+        print("Name : ", self.name  ,"Salary : ", self.salary)
+
+
+
+emp1 = Employee("Ernest", 5000000)
+emp2 = Employee("Henry", 10000000)
+
+emp1.displayEmployee()
+emp2.displayEmployee()
+
+print("Total  employee %d" % Employee.EmpCount)
